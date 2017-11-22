1110b32ab3dfd881e2df1da5783682e0383d6a22
fshiper <przemyslaw.krzywodajc@gmail.com>
2017-11-22 22:54:57 +0100

    Creating a Service to Get Album Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-import { ProductService } from 'app/product.service';
+import { ProductService } from './product.service';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,6 @@ import { ProductService } from 'app/product.service';
     ProductDescriptionComponent
   ],
   imports: [
-    ProductService,
     BrowserModule,
     FormsModule,
     HttpModule
